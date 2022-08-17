c1cf25801282b3c1e47d4c0270285ce6997a30da
Nur Luski Levi <BritishPirate@users.noreply.github.com>
2022-08-17 17:16:09 +0100

    Successfully animated email logo

---
+++ index.js
@@ -0,0 +1,16 @@
+function contactButtons(id){
+  anime({
+    targets: '#' + id,
+    translateX: 100
+  })
+
+}
+
+anime({
+  targets: '',
+  translateX: 250,
+  rotateZ: 360,
+  scale: 3,
+  duration: 3000,
+  loop: true
+});
